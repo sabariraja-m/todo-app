6ddaa4d382dc07373bd2e7cfc2b40a93d0d67343
sabariraja-m <sabariiyappan1118@gmail.com>
2024-10-15 12:58:46 +0530

    added test env

---
+++ src/components/Button/button.test.tsx
@@ -0,0 +1,11 @@
+import { describe, expect, test } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+import { Button } from "./index";
+
+describe("Button Test", () => {
+    test("should render button", () => {
+        render(<Button onClick={() => {}}>Click me</Button>);
+        const element = screen.getByTestId("button-test");
+        expect(element).toBeTruthy();
+    });
+});
