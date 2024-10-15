3eeb74eabf0684f1c0d7ab876c01101984629de9
sabariraja-m <sabariiyappan1118@gmail.com>
2024-10-15 14:06:43 +0530

    evil commit

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -3,6 +3,15 @@ interface ButtonProps {
 }
 export const Button = (props: ButtonProps) => {
     const { onClick } = props;
+    const handler = () => {
+        console.log("hey");
+    };
+    if (onClick)
+        return (
+            <Button onClick={handler} data-testid="button-test">
+                button
+            </Button>
+        );
     return (
         <button onClick={onClick} data-testid="button-test">
             button
