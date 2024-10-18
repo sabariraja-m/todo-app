9cc20d0811022e37d3282e72f671b337c1cacda2
sabariraja-m <sabariiyappan1118@gmail.com>
2024-10-18 10:31:32 +0530

    storybook setup

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,14 +1,17 @@
-"use client";
-import { ReactNode } from "react";
+"use client"
+
+import { ReactNode } from "react"
+
 interface ButtonProps {
-    onClick: () => void;
-    children: ReactNode;
+    onClick?: () => void,
+    primary?: Boolean,
+    label?: String,
+    size?: String,
 }
 export const Button = (props: ButtonProps) => {
-    const { onClick, children } = props;
+    const { onClick, label } = props
+
     return (
-        <button onClick={onClick} data-testid="button-test">
-            {children}
-        </button>
-    );
-};
+        <button onClick={onClick} data-testid="button-test">{label}</button>
+    )
+}
